51073e6b40a707b4c20ce978798de95e0271ddfd
Priyal Samaratunga <priyal.samaratunga@cambio.lk>
2020-07-10 00:27:09 +0530

    Added store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NavbarComponent
-  ],
+  declarations: [AppComponent, HomeComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ maxAge: 10 })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/customers/state/customer.actions.ts
@@ -0,0 +1,23 @@
+import { Action } from '@ngrx/store';
+import { Customer } from '../customer.model';
+
+
+export enum CustomerActionTypes{
+  LOAD_CUSTOMERS = '[Customer] Load Customers',
+  LOAD_CUSTOMERS_SUCCESS = '[Customer] Load Customers Success',
+  LOAD_CUSTOMERS_FAIL = '[Customer] Load Customers Fail'
+}
+
+export class LoadCustomers implements Action{
+  readonly type = CustomerActionTypes.LOAD_CUSTOMERS;
+}
+export class LoadCustomersSuccess implements Action{
+  readonly type = CustomerActionTypes.LOAD_CUSTOMERS_SUCCESS;
+  constructor(public payload : {customers :Customer[]}){}
+}
+export class LoadCustomersFail implements Action{
+  readonly type = CustomerActionTypes.LOAD_CUSTOMERS_FAIL;
+  constructor(public payload :string){}
+}
+
+export type Action = LoadCustomers | LoadCustomersSuccess | LoadCustomersFail;
